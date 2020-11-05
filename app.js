80af31ea1930ed63b8fde5b617f5a1d2f9356bd7
Tamal <tamaldn2000@gmail.com>
2020-11-05 23:53:44 +0530

    page footer, jumbotrons added, transition effect customized

---
+++ app.js
@@ -0,0 +1,15 @@
+function toggleText() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("paraToggle");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
